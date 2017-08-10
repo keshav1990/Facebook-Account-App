82c93087aa7f36a1c3d7362952598bc69d825dab
Keshav Kalra <keshavkalra1990@gmail.com>
2017-08-10 16:54:10 +0530

    database
    db

---
+++ multi_system/fb_app (3).sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 10, 2017 at 01:22 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fb_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_login`
+--
+
+CREATE TABLE `admin_login` (
+  `admin_id` int(100) NOT NULL,
+  `admin_name` varchar(50) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_password` varchar(250) NOT NULL,
+  `is_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_login`
+--
+
+INSERT INTO `admin_login` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, `is_admin`) VALUES
+(1, 'admin1', 'admin@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 1),
+(2, 'abc', 'admin1@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 0),
+(3, 'keshav', 'keshav@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 0),
+(4, 'abc123', 'abc123@gmail.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_account`
+--
+
+CREATE TABLE `manage_account` (
+  `id` int(100) NOT NULL,
+  `account_name` varchar(50) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `total_leads` int(100) NOT NULL,
+  `account_status` varchar(10) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `manage_account`
+--
+
+INSERT INTO `manage_account` (`id`, `account_name`, `account_id`, `total_leads`, `account_status`, `time`, `is_deleted`, `user_id`) VALUES
+(1, 'abcc', 324, 1004, '0', '2017-08-02 04:29:36', 1, 1),
+(2, 'pqr', 232, 10032, '0', '2017-08-01 12:20:06', 0, 2),
+(3, 'dfdf', 0, 100, '0', '2017-08-01 12:23:21', 0, 2),
+(4, 'abcb', 74, 10034, '0', '2017-08-01 13:14:25', 0, 3),
+(5, 'sadfdf', 0, 100, '0', '2017-08-02 04:31:13', 0, 1),
+(6, 'f', 0, 100, '0', '2017-08-02 04:30:03', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `manage_account`
+--
+ALTER TABLE `manage_account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  MODIFY `admin_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `manage_account`
+--
+ALTER TABLE `manage_account`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
